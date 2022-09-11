11e0864fe8c85b906999ef35a55e197016307e16
Shota <1s2b1acksn0w@gmail.com>
2022-09-11 23:19:27 +0900

    ユーザー登録、ログイン、ログアウト機能追加とビューのビューの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-  validates :name,        presence: true
+  validates :name, presence: true
 end
