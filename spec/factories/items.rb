25f83d00bea5410547c97841a99667c09dee29d1
Shota <1s2b1acksn0w@gmail.com>
2022-09-12 19:26:16 +0900

    アイテムモデル単体テストコード記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name                   { Faker::Name.name }
+    content                { Faker::Lorem.sentence }
+    privacy_id            { Faker::Number.between(from: 2, to: 3) }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  describe '新規投稿' do
+    context '投稿できるとき' do
+      it '必要な情報を適切に入力して「投稿する」ボタンを押すと、アイテム情報がデータベースに保存される' do
+        expect(@item).to be_valid
+      end
+
+      it '画像がなくても投稿できる' do
+        @item.image = nil
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品情報を登録できないとき' do
+      it 'アイテム名が空では投稿できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it 'アイテム内容が空では投稿できない' do
+        @item.content = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Content can't be blank")
+      end
+
+      it '公開範囲の情報が空では投稿できない' do
+        @item.privacy_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Privacy can't be blank")
+      end
+
+      it '公開範囲に「---」が選択されている場合は投稿できない' do
+        @item.privacy_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Privacy can't be blank")
+      end
+
+      it 'userが紐付いていなければ投稿できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
