f0a3b7a7178f02e02fd170c0ff5287d7dec797d6
Shota <1s2b1acksn0w@gmail.com>
2022-09-12 19:03:15 +0900

    新規投稿機能と投稿一覧表示機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :privacy
   belongs_to :user
+  has_one_attached :image
 
   validates :name,       presence: true
   validates :content,    presence: true
