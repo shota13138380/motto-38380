84b4ec92ae63df04e3416de5f32f7d08af7a3830
Shota <1s2b1acksn0w@gmail.com>
2022-09-13 16:38:17 +0900

    記述ミス修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,7 +39,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = item.find(params[:id])
+    item = Item.find(params[:id])
     item.destroy
     redirect_to root_path
   end
