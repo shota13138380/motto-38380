4c6b873503e61ced4f8352aa55f9ec0e1f3d4a50
Shota <1s2b1acksn0w@gmail.com>
2022-09-13 15:45:28 +0900

    アクション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, onry: [:show, :edit, :update]
-  
+
   def index
     @items = Item.order('created_at DESC')
   end
@@ -20,15 +19,18 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
     unless current_user.id == @item.user_id
       redirect_to root_path
     end
   end
 
   def update
+    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -46,9 +48,5 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :content, :privacy_id, :image).merge(user_id: current_user.id)
-  end
-
-  def set_item
-    @item = Item.find(params[:id])
   end
 end
