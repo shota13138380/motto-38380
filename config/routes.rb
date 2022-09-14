195a317f21892f04acc5d3c76a74032229624fb5
Shota <1s2b1acksn0w@gmail.com>
2022-09-14 19:00:27 +0900

    アクション定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :users, only: [:show, :edit, :update, :destroy]
-  resources :items
+  resources :items do
+    resources :comments, only: [:create]
+  end
 end
