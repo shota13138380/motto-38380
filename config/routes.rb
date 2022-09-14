c0b28ea8b0bd8b6573a587a40b9a9e23b86ec996
Shota <1s2b1acksn0w@gmail.com>
2022-09-14 16:55:54 +0900

    退会機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :destroy]
   resources :items
 end
