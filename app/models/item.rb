a3282699e46d7f271e847a2577fcdc9e12756acb
Shota <1s2b1acksn0w@gmail.com>
2022-09-16 12:41:06 +0900

    抜けていたコードを追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many   :comments, dependent: :destroy
   has_one_attached :image
-  has_many :item_tags
+  has_many :item_tags, dependent: :destroy
   has_many :tags, through: :item_tags
 
 end
